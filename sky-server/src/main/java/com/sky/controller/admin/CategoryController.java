2cadf6a1f49e11a911a778f2012ee16f9f87bf2f
jingxu-i-xiaozhou <jingxushi13@gmail.com>
2025-08-20 08:43:01 +0800

    开始开发分类管理模块

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,15 @@
+package com.sky.controller.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 24053
+ */
+@Slf4j
+@RestController
+@RequestMapping("/admin/setmeal")
+public class CategoryController {
+
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,12 @@
+package com.sky.service.impl;
+
+import com.sky.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 24053
+ */
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+}
